de57fbc99fc81dac7c7bb37b57d9be9f148333d8
Viacheslav Diukov <slavad156@ukr.net>
2022-01-14 15:52:28 +0200

    finishes task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const allItemsRef = ingredients.map(ingredient => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = ingredient;
+  itemRef.classList.add('item');
+
+  return itemRef;
+});
+
+const allIngredients = document.querySelector('#ingredients');
+
+allIngredients.append(...allItemsRef);
