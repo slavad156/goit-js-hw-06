e007b0ff64d6d10e7a682866eec8160972a392d6
Viacheslav Diukov <slavad156@ukr.net>
2022-01-15 20:38:33 +0200

    finishes task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector('.login-form');
 
+const onFormSubmit = function (event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All fields must be filled!');
+  }
+  const data = { email, password };
+
+  console.log(data);
+  event.currentTarget.reset();
+};
+
+formRef.addEventListener('submit', onFormSubmit);
