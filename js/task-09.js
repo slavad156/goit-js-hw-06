457f6b881c93f7e917b1a917bef0f9ce6b7edb9e
Viacheslav Diukov <slavad156@ukr.net>
2022-01-15 20:41:12 +0200

    finishes task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
+const getRandomHexColor = function () {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const bodyRef = document.querySelector('body');
+const btnRef = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+const randomColor = function () {
+  currentColor.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = currentColor.textContent;
+};
+
+btnRef.addEventListener('click', randomColor);
