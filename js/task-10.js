ca87e629d590e3bebe0c25781db5a9bd13c38a21
Viacheslav Diukov <slavad156@ukr.net>
2022-01-15 20:42:33 +0200

    finishes task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
-function getRandomHexColor() {
+const getRandomHexColor = function () {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refs = {
+  inputRef: document.querySelector('#controls input'),
+  btnCreateRef: document.querySelector('[data-create]'),
+  btnDestroyRef: document.querySelector('[data-destroy]'),
+  boxesRef: document.querySelector('#boxes'),
+};
+
+const createBoxes = function (amount) {
+  let startSize = 30;
+  const arrBox = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    let size = startSize + i * 10;
+
+    const createDiv = document.createElement('div');
+    createDiv.style.width = `${size}px`;
+    createDiv.style.height = `${size}px`;
+    createDiv.style.backgroundColor = getRandomHexColor();
+    arrBox.push(createDiv);
+  }
+
+  return refs.boxesRef.append(...arrBox);
+};
+
+const getAmount = function () {
+  createBoxes(Number(refs.inputRef.value));
+};
+const destroyBoxes = function () {
+  refs.boxesRef.innerHTML = '';
+};
+
+refs.btnCreateRef.addEventListener('click', getAmount);
+refs.btnDestroyRef.addEventListener('click', destroyBoxes);
